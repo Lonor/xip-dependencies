7932e84af2b3b417bb62484c13f9c40dd656e7b3
Lawrence Li <lonor@live.com>
2022-03-10 20:56:32 +0800

    ci: add deps yml

--- src/main/java/me/lawrenceli/Main.java
+++ src/main/java/me/lawrenceli/Main.java
@@ -1,7 +1,10 @@
 package me.lawrenceli;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BlackDuck!");
+        log.info("Hello BlackDuck!");
     }
 }
